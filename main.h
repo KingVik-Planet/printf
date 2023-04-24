63f7f22957b153dfeac09a2f843ab2c09ad70e4d
kofoworola <kofoworolao11@gmail.com>
2023-04-24 14:26:23 -0700

    kingsley and kofoworola

--- main.h
+++ main.h
@@ -2,39 +2,31 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
-<<<<<<< HEAD
 #include <std.h>
 
 #define UNSUED(x) (void)(x)
-=======
 #include <unistd.h>
 
 #define UNUSED(x) (void)(x)
->>>>>>> bda9717927f85d020f95ca60a9ee00df015d8c00
 #define BUFF_SIZE 1024
 
 /* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
-<<<<<<< HEAD
 #define F_HARSH 8
 #define F_SPACE 16
 
 /* SIZE */
-=======
 #define F_HASH 8
 #define F_SPACE 16
 
 /* SIZES */
->>>>>>> bda9717927f85d020f95ca60a9ee00df015d8c00
 #define S_LONG 2
 #define S_SHORT 1
 
 /**
-<<<<<<< HEAD
  * struct fmt _Struct op
-=======
  * struct fmt - Struct op
  * by kingsley and kofoworola
  *
